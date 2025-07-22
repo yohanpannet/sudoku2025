7c763bf100dd1646126acd8903aed0e1e8b097c3
yohanpannet <yohan.pannet@gmail.com>
2025-07-22 18:57:46 +0200

    1st display of sudoku grid

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AssetReader } from './asset-reader';
 import { MatSelectModule } from '@angular/material/select';
-import { SudokuGrid } from './model/SudokuCell';
+import { emptySudokuGrid, SudokuGrid } from './model/SudokuCell';
 
 @Component({
   selector: 'app-root',
@@ -18,12 +18,14 @@ export class App implements OnInit{
   protected readonly title = signal('sudoku2025');
 
   protected grids: SudokuGrid[] = [];
+  protected selectedGrid: SudokuGrid = emptySudokuGrid;
 
   ngOnInit(): void {
     this.assetReader.getGridList().subscribe(grids => this.grids = grids);
   }
 
   onGridSelectionChange(grid: SudokuGrid) {
-    console.log(grid)
+    console.log(grid);
+    this.selectedGrid = grid;
   }
 }
--- src/app/model/SudokuCell.ts
+++ src/app/model/SudokuCell.ts
@@ -6,3 +6,18 @@ export interface SudokuCell {
 export interface SudokuGrid {
     cells: SudokuCell[];
 }
+
+export const emptySudokuGrid: SudokuGrid = getEmptyGrid();
+
+function getEmptyGrid(): SudokuGrid {
+    let grid: SudokuGrid = {
+        cells: []
+    }
+    for (let i = 0; i<81; i++) {
+        grid.cells.push({
+            value: 0,
+            startsValues: false
+        })
+    }
+    return grid;
+}
